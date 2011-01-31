5f0a7ba20ab12219876a1d92892ff6915cb59d8b
Mikhail Tabunov <tmp.over@gmail.com>
2011-01-31 17:45:02 +0300

    Initial

---
+++ init.rb
@@ -0,0 +1,16 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_issue_template do
+  name 'Redmine Issue Template'
+  author 'undev.ru'
+  description 'Allows to manage templates of issues, and adding them to issue body'
+  version '0.0.1'
+  url 'http://undev.ru'
+  author_url 'http://undev.ru'
+end
+
+
+ApplicationHelper.send(:include, IssueTemplatesHelper)
+require 'redmine_issue_template/hooks/view_issues_form_details_bottom_hook'
+
+
